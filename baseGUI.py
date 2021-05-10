7e48a0caabab862b938c68b85aa64ed2b656254d
huroshokunin <1CJK1207@cc.u-tokai.ac.jp>
2021-05-10 22:37:13 +0900

    基礎的なGUIと機能のみを追加 ツイートの為の内部処理は「Twitter社がAPI仕様を許可しない限り『永遠と』目的が達成できません。」
    Only basic GUI and functionality added.
    Internal processing for tweets "will not achieve its purpose 'forever' unless Twitter allows the API specification.
    
    I don't speak English, so I think my English is weird.

--- baseGUI.py
+++ baseGUI.py
@@ -1,3 +1,40 @@
 import tkinter
-import tkinter.ttk
+from tkinter import ttk, filedialog
 
+def askfolder():
+    #参照ボタン機能
+    path = filedialog.askopenfilename()
+    f_path.set(path)
+
+def do_Tweet():
+    pass
+
+#GUIの色々
+m_window = tkinter.Tk()
+m_window.title("TEST")
+m_window.geometry("400x200")
+
+m_frame = ttk.Frame(m_window)
+m_frame.grid(column=0, row=0, sticky=tkinter.NSEW, padx=5, pady=10)
+
+
+f_path = tkinter.StringVar()
+
+#ウィジェット追加
+f_label = ttk.Label(m_frame, text="folder")
+f_box = ttk.Entry(m_frame, textvariable=f_path)
+f_button = ttk.Button(m_frame, text="reference", command=askfolder)
+T_button = ttk.Button(m_frame, text="Tweet!", command=do_Tweet)
+
+
+#ボタン配置
+f_label.grid(column=0, row=0, pady=10)
+f_box.grid(column=1, row=0, sticky=tkinter.EW, padx=5)
+f_button.grid(column=2, row=0)
+T_button.grid(column=2, row=1)
+
+m_window.columnconfigure(0, weight=1)
+m_window.rowconfigure(0, weight=1)
+m_frame.columnconfigure(1, weight=1)
+
+m_window.mainloop()
